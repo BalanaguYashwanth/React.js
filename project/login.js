5d25446454fc5969c4ab72289e06fa2c2f2d320d
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-03-13 23:54:28 +0530

    Add files via upload

--- project/login.js
+++ project/login.js
@@ -1,39 +1,57 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import fb from '../firebase'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
-export default function login(){
+export default function login() {
     const history = useHistory()
 
-    const [login,setLogin] = useState({username:'',password:''})
-    const [response,setResponse] = useState()
+    const [login, setLogin] = useState({ username: '', password: '' })
+    const [response, setResponse] = useState()
 
-    function submitting(){
-        axios.post('http://127.0.0.1:8000/login',{
-           username:login.username,
-           password:login.password,
-        })
-        .then(res=>{
-            console.log(res.data)
-            localStorage.setItem('token',res.data.token)
-            history.push('/profile')
-        })
-        .catch(err=>{
-            console.log(err.message)
-        })
+    function submitting() {
+        if(login.username!='' && login.password!='')
+        {
+            axios.post('http://127.0.0.1:8000/login', {
+                username: login.username,
+                password: login.password,
+            })
+                .then(res => {
+                    console.log(res.data)
+                    localStorage.setItem('token', res.data.token)
+                    history.push('/profile')
+                })
+                .catch(err => {
+                    setResponse(err.response.data.user)
+                    console.log(err.response.data)
+                })
+        }else{
+            setResponse('please enter the valid details')
+        }
+       
     }
 
-    return(
+    return (
         <div>
-            <p>  Login </p>
-            <input type="text" placeholder="enter the username" onChange={ (e) => ( setLogin({...login,username:e.target.value}) )} />
-            <br />
-            <input type="password" placeholder="enter the password" onChange={ (e) => ( setLogin({...login,password:e.target.value}) )}  />
-            <br />
-            <button onClick={submitting} > submit </button>
-            <br />
-            {response}
+            <div id="section" >
+                <div id="body">
+                    <div id="form" className="justify-content-center">
+                        <div id="profilenamelogin" className="mx-auto" >
+                        
+                        <i className="fa fa-users mx-auto my-2" style={{ fontSize: 80, textAlign: 'center' }}> <p style={{ fontStyle: 'oblique' }} > Login </p> </i>
+                        </div>
+                        <div className="form-row"  >
+                            <div className="form-group  col-md " >
+                                <input type="text" placeholder="enter the Username" className="form-control my-3"  onChange={(e) => (setLogin({ ...login, username: e.target.value }))} />
+                                <input type="password" placeholder="enter the Password" className="form-control my-3" onChange={(e) => (setLogin({ ...login, password: e.target.value }))} />
+                                <button className="mx-auto btn btn-light" onClick={submitting} id="button"  > submit </button>
+                                <div style={{ textAlign: 'center', alignItems: 'center' }}>
+                                {response}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     )
--- project/register.js
+++ project/register.js
@@ -1,47 +1,54 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import fb from '../firebase'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
-export default function Register(){
+export default function Register() {
     const history = useHistory();
 
-    const [register,setRegister] = useState({username:'',email:'',password:'',first_name:'',last_name:''})
-    const [response,setResponse] = useState()
+    const [register, setRegister] = useState({ username: '', email: '', password: '', first_name: '', last_name: '' })
+    const [response, setResponse] = useState()
 
-    function submitting(){
+    function submitting() {
         console.log(register)
-       axios.post('http://127.0.0.1:8000/register',{
-           username:register.username,
-           first_name:register.first_name,
-           last_name:register.last_name,
-           password:register.password,
-           username:register.username,
-           email:register.email,
-       })
-       .then(res=>{
-           console.log(res)
-            history.push('/login')
+        axios.post('http://127.0.0.1:8000/register', {
+            username: register.username,
+            first_name: register.first_name,
+            last_name: register.last_name,
+            password: register.password,
+            username: register.username,
+            email: register.email,
         })
-       .catch(err=>console.log(err.response))
+            .then(res => {
+                console.log(res)
+                history.push('/login')
+            })
+            .catch(err => console.log(err.response))
     }
 
-    return(
+    return (
         <div>
-            <p>  Register </p>
-            <input type="text" placeholder="enter the first name" onChange={ (e) => ( setRegister({...register,first_name:e.target.value}) )} />
-            <br />
-            <input type="text" placeholder="enter the last name" onChange={ (e) => ( setRegister({...register,last_name:e.target.value}) )} />
-            <br />
-            <input type="text" placeholder="enter the username" onChange={ (e) => ( setRegister({...register,username:e.target.value}) )} />
-            <br />
-            <input type="email" placeholder="enter the email" onChange={ (e) => ( setRegister({...register,email:e.target.value}) )} />
-            <br />
-            <input type="password"  placeholder="enter the password" onChange={ (e) => ( setRegister({...register,password:e.target.value}) )}  />
-            <br />
-            <button onClick={submitting} > submit </button>
-            <br />
-            {response}
+            <div id="section" >
+                <div id="body">
+                    <div id="form" className="justify-content-center">
+                        <div id="profilenamelogin" className="mx-auto " >
+                            <i className="fa fa-users mx-auto my-2" style={{ fontSize: 80, textAlign: 'center' }}> <p style={{ fontStyle: 'oblique' }} > Register </p> </i>
+                        </div>
+                        <div className="form-row"  >
+                            <div className="form-group  col-md " >
+                                <input type="text" placeholder="enter the first name" className="form-control my-3"  onChange={(e) => (setRegister({ ...register, first_name: e.target.value }))} />
+                                <input type="text" placeholder="enter the last name" className="form-control my-3" onChange={(e) => (setRegister({ ...register, last_name: e.target.value }))} />
+                                <input type="text" placeholder="enter the username" className="form-control my-3" onChange={(e) => (setRegister({ ...register, username: e.target.value }))} />
+                                <input type="email" placeholder="enter the email" className="form-control my-3" onChange={(e) => (setRegister({ ...register, email: e.target.value }))} />
+                                <input type="password" placeholder="enter the password" className="form-control my-3" onChange={(e) => (setRegister({ ...register, password: e.target.value }))} />
+                                <button onClick={submitting} className="mx-auto btn btn-light my-2"  id="button" > submit </button>
+                                {response}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
